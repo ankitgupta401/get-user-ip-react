a76697dc861575095a67d107e36a93ba2d1d4063
Ankit Gupta <ag3831124@gmail.com>
2022-08-28 21:19:09 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [ip, setIp] = useState("");
+  useEffect(() => {
+    getUserIp();
+  }, []);
+
+  const getUserIp = async () => {
+    const ip = await axios.get("https://ipapi.co/json");
+    console.log(ip.data);
+    setIp(ip.data.ip);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>IP Address: {ip}</h1>
     </div>
   );
 }
